404b8aa2de84f21ddceb41040887a4b1fe5933f2
Denys Pachekhon <denyspa@compatibl.com>
2021-10-21 17:38:49 +0200

    WEB-NOTES-3: renaming project and solution name

--- backend/Notes.WebAPI/Program.cs
+++ backend/Notes.WebAPI/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace NotesAPI
+namespace Notes.WebAPI
 {
     public class Program
     {
--- backend/Notes.WebAPI/Startup.cs
+++ backend/Notes.WebAPI/Startup.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace NotesAPI
+namespace Notes.WebAPI
 {
     public class Startup
     {

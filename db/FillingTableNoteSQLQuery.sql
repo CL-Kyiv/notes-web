2cb06bdd658027f1046099fa0f8edfb880e61f00
Denys Pachekhon <denyspa@compatibl.com>
2021-10-21 17:07:25 +0200

    WEB-NOTES-3: filling table 'note'

---
+++ db/FillingTableNoteSQLQuery.sql
@@ -0,0 +1,15 @@
+use test_db
+
+insert into note
+values (1,'note1','note1',GETDATE()),
+(2,'note2','note2',GETDATE()),
+(3,'note3','note3',GETDATE()),
+(4,'note4','note4',GETDATE()),
+(5,'note5','note5',GETDATE()),
+(6,'note6','note6',GETDATE()),
+(7,'note7','note7',GETDATE()),
+(8,'note8','note8',GETDATE()),
+(9,'note9','note9',GETDATE()),
+(10,'note10','note10',GETDATE()),
+(11,'note11','note11',GETDATE()),
+(12,'note12','note12',GETDATE());

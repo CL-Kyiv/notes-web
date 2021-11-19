9ec021676d5cab136c83b65b4756e4bccfcd4b90
Oleksandr Kravchuk <olexanderk@compatibl.com>
2021-11-19 11:22:04 +0100

    WEB-NOTES-22: Fix build. Implement data refresh trigger

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,42 +2,37 @@ import { Component } from '@angular/core';
 import { NoteService } from './note.service';
 import { ColDef } from 'ag-grid-community';
 import { Note } from './note.type';
-import { Observable, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { switchMap, tap } from 'rxjs/operators';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { NoteDialogComponent } from './note-dialog/note-dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
-  private gridApi : any;
+  private gridApi: any;
   rowHeight = 50;
 
-  constructor(private service: NoteService,
-    private matDialog : MatDialog) {}
+  constructor(private service: NoteService, private matDialog: MatDialog) {}
 
   refreshData$: Subject<void> = new Subject<void>();
-  notesData:  Note[] = [];
+  notesData: Note[] = [];
 
-  rowIsSelected : boolean = false;
+  rowIsSelected: boolean = false;
 
-  selectedData : any;
-  
+  selectedData: any;
+
   onGridReady(params: any) {
     this.gridApi = params.api;
     this.gridApi.sizeColumnsToFit();
-    const api$ = this.refreshData$
-    .pipe(
-      switchMap(() => {
-        return this.service.getNotes();
-      },
-      tap(notes=> this.notesData = notes ))
+    const api$ = this.refreshData$.pipe(
+      switchMap(() => this.service.getNotes()),
+      tap((notes) => (this.notesData = notes))
     );
-    api$.subscribe(()=>{});
+    api$.subscribe(() => {});
     this.fetchNotes();
   }
 
@@ -45,30 +40,30 @@ export class AppComponent {
     this.refreshData$.next();
   }
 
-  openEditDialog(){
-    this.matDialog.open(NoteDialogComponent, {data : this.selectedData});
+  openEditDialog() {
+    this.matDialog.open(NoteDialogComponent, { data: this.selectedData });
   }
- 
+
   columnDefs: ColDef[] = [
-    { 
+    {
       field: 'title',
       editable: true,
       cellEditor: 'agLargeTextCellEditor',
-      width: 30
-    }
+      width: 30,
+    },
   ];
 
   onRowClick() {
     let selectedNodes = this.gridApi.getSelectedNodes();
-    this.selectedData = selectedNodes.map((node : any) => node.data)[0];
+    this.selectedData = selectedNodes.map((node: any) => node.data)[0];
     this.rowIsSelected = true;
   }
 
-  onDeleteNode(){
+  onDeleteNode() {
     this.service.deleteNote(this.selectedData.id);
   }
 
-  onCreateNode(){
+  onCreateNode() {
     this.service.createNote();
   }
 }
